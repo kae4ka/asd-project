c430802f97d761a6d41b67ae044d2fdfd645d4be
Artem Zhulin <70791841+TVI-ARTEM@users.noreply.github.com>
2024-11-05 10:38:57 +0300

    Update main.py

--- task_8/Zhulin Artem/kwic/main.py
+++ task_8/Zhulin Artem/kwic/main.py
@@ -1,33 +1,67 @@
+class Filter:
+    def execute(self, input_lines: list[str]) -> list[str]:
+        pass
+
+
+class ReadFilter(Filter):
+    def execute(self, input_lines: list[str]) -> list[str]:
+        return [line.strip() for line in input_lines]
+
+
+class CircularShiftFilter(Filter):
+    def execute(self, input_lines: list[str]) -> list[str]:
+        for line in input_lines:
+            words = line.split()
+            for index in range(len(words)):
+                yield ' '.join(words[index:] + words[:index])
+
+
+class AlphabetizerFilter(Filter):
+    def execute(self, input_lines: list[str]) -> list[str]:
+        return sorted(input_lines)
+
+
+class OutputFilter(Filter):
+    def execute(self, input_lines: list[str]) -> list[str]:
+        return input_lines
+
+
+class Pipe:
+    def __init__(self):
+        self.filters: list[Filter] = []
+
+    def add(self, new_filter: Filter):
+        self.filters.append(new_filter)
+        return self
+
+    def execute(self, input_lines: list[str]) -> list[str]:
+        for executable_filter in self.filters:
+            input_lines = executable_filter.execute(input_lines)
+
+        return input_lines
+
+
 def main():
-    lines = input_filter()
-    lines = circular_shit_filter(lines)
-    lines = alphabetizer_filter(lines)
-    output_filter(lines)
+    pipe = Pipe()
+    pipe.add(ReadFilter()).add(CircularShiftFilter()).add(AlphabetizerFilter()).add(OutputFilter())
+
+    lines = user_input()
+    lines = pipe.execute(lines)
+    user_output(lines)
 
 
-def input_filter() -> list[str]:
+def user_input() -> list[str]:
     while True:
         input_line = input("Enter text (empty line to stop): ").strip()
 
         if input_line == '':
             break
 
-        yield input_line.strip()
+        yield input_line
 
 
-def circular_shit_filter(input_lines: list[str]) -> list[str]:
-    for line in input_lines:
-        words = line.split()
-        for index in range(len(words)):
-            yield ' '.join(words[index:] + words[:index])
-
-
-def alphabetizer_filter(input_lines: list[str]) -> list[str]:
-    return sorted(input_lines)
-
-
-def output_filter(input_lines: list[str]):
-    for line in input_lines:
+def user_output(lines: list[str]):
+    for line in lines:
         print(line)
 
 
